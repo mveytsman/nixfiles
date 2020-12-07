a1594e8aa406405f5ea5c0012973ea7207c37cc4
Max Veytsman <maxim@ontoillogical.com>
2020-12-06 21:39:47 -0500

    Add some NAS stuff

--- common/system.nix
+++ common/system.nix
@@ -19,8 +19,20 @@
   # Common networking
   networking.extraHosts = ''
     155.138.147.72 marquez
+    192.168.2.15 vidal
   '';
   
+  fileSystems."/mnt/vidal/includesec" = {
+      device = "//vidal/includesec";
+      fsType = "cifs";
+      options = let
+        permissions_opts = "uid=1000,gid=100";
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+        credentials_opts = "credentials=/etc/nixos/secrets/smb_secrets";
+      in ["${permissions_opts},${automount_opts},${credentials_opts}"];
+  };
+
   networking.nameservers = [ "1.1.1.1" ];
 
   # This value determines the NixOS release with which your system is to be
