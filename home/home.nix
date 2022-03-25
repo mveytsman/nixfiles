5a6298a0865e6dfc5c2632069b6710c17b185399
Max Veytsman <maxim@ontoillogical.com>
2022-03-24 22:43:13 -0400

    Fix hom

--- home/home.nix
+++ home/home.nix
@@ -27,17 +27,15 @@
     lm_sensors
     upower
     rofi
-    clipmenu
-    clipnotify
     maim
     xclip
 
     # programming
-    ag
+    silver-searcher
     alacritty
     vim
     emacs
-    vscode
+    vscode-with-extensions.override { vscodeExtensions = with vscode-extensions; [ ms-vsliveshare.vsliveshare ]; }
     cmake
     nixfmt
 
@@ -78,23 +76,6 @@
   };
 
   # config
-
   home.file.".config/i3/config".source = ./config/i3/config;
-    home.file.".config/i3//i3status-rs.toml".source = ./config/i3/i3status-rs.toml;
-
-
-  # setup vscode for live share
-  imports = [
-    #./i3.nix
-    "${
-      fetchTarball "https://github.com/msteen/nixos-vsliveshare/tarball/master"
-    }/modules/vsliveshare/home.nix"
-  ];
-
-  services.vsliveshare = {
-    enable = true;
-    extensionsDir = "$HOME/.vscode/extensions";
-    nixpkgs = fetchTarball
-      "https://github.com/NixOS/nixpkgs/tarball/61cc1f0dc07c2f786e0acfd07444548486f4153b";
-  };
+  home.file.".config/i3//i3status-rs.toml".source = ./config/i3/i3status-rs.toml;
 }
